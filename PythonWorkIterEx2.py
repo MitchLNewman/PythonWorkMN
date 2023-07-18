8f334ce7ceba9481bbd6c98952579ddfdbeaed43
Mitch L Newman <mitchellnewman3@gmail.com>
2023-07-18 16:00:51 +0100

    Iteration Exercises

---
+++ PythonWorkIterEx2.py
@@ -0,0 +1,15 @@
+
+# While loop for 5 names and adding is awesome to end
+
+count = 0 
+
+while count < 5:
+    name = input("Please enter a name: ")
+    print(name + " Is Awesome!")
+    count += 1
+
+# for loop explaination exercise 
+#It prints each number in the range from 10 to 20, whilst incrementing by 2 at each iteration
+for i in range(10, 21, 2):
+    print(i)
+
