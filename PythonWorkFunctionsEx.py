b9a0d05e498e5a5a5b5f005c6b942ac13293b416
Mitch L Newman <mitchellnewman3@gmail.com>
2023-07-19 10:46:00 +0100

    Added Citations

--- PythonWorkFunctionsEx.py
+++ PythonWorkFunctionsEx.py
@@ -1,12 +1,15 @@
 
+# Function that uses if statements and inputs to display final grade and percentage
 def finalgrade(name, homework, assessment, finalExam, grade):
     return f" {name}, your final ICT percentage is {calc}%. Your grade is {grade}"
 
+# Inputs needed for grade calculations 
 name = input("Input Student Name: ")
 homework = int(input("Input Student Homework Score: "))
 assessment = int(input("Input Student Assessment Score: "))
 finalExam = int(input("Input Student Final Exam Score: "))
 
+# If statements to calculate what percentage would be what grade
 calc = int((homework + assessment + finalExam)/175*100)
 if calc >= 80:
         grade = "A"
