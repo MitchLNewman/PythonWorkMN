1843d5daa6d96f6ddfcf3e67716033b645c40c1d
Mitch L Newman <mitchellnewman3@gmail.com>
2023-07-19 15:59:41 +0100

    W3 Function exercises 1-4

---
+++ PythonWorkW3functEx.py
@@ -0,0 +1,39 @@
+#  Exercise 1 Find the Max of three numbers
+
+def max_of_two( x, y ):
+    if x > y:
+        return x
+    return y
+def max_of_three( x, y, z ):
+    return max_of_two( x, max_of_two( y, z ) )
+print(max_of_three(3, 6, -5))
+
+#  Exercise 2 Find the sum of all the numbers in a list
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+#  Exercise 3 same as two but multiplying
+
+def multiply(numbers):  
+    total = 1
+    for x in numbers:
+        total *= x  
+    return total  
+print(multiply((8, 2, 3, -1, 7)))
+
+#  Exercise 4 reverse a string 
+
+def string_reverse(str1):
+
+    rstr1 = ''
+    index = len(str1)
+    while index > 0:
+        rstr1 += str1[ index - 1 ]
+        index = index - 1
+    return rstr1
+print(string_reverse('HelloWorld'))
