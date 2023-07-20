0e965891d02f1abadd290ec1fc652215949aa36b
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 <mitchellnewman3@gmail.com>
2023-07-20 16:20:59 +0100

    reads and prints 1st team but not 4th

--- PythonWorkFileEx.py
+++ PythonWorkFileEx.py
@@ -4,3 +4,11 @@ with open("teams.txt", "w") as file:
     for item in n:
         file.write("%s\n" % item)
     print("Done")
+
+file = open("teams.txt", "r")
+lines = file.readlines(1)
+print(lines)
+file.close()
+
+# get readlines to print 1st and 4th line somehow
+# first line complete
