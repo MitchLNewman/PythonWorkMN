fe480f1b30b4d5ce9f114a1fa6469da91c96f89f
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 <mitchellnewman3@gmail.com>
2023-07-20 15:59:00 +0100

    Files Exercise

---
+++ PythonWorkFileEx.py
@@ -0,0 +1,8 @@
+file = open("teams.txt", "r")
+
+n = ["ManUtd", "ManCity", "Arsenal", "Chelsea","Liverpool"]
+
+with open("teams.txt", "w") as file:
+    for item in n:
+        file.write("%s\n" % item)
+    print("Done")
