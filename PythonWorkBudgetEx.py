36817d5f4301b728e64800c167da75904eff5167
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 <mitchellnewman3@gmail.com>
2023-07-20 12:08:49 +0100

    Added inputs for clothing and entertainment

--- PythonWorkBudgetEx.py
+++ PythonWorkBudgetEx.py
@@ -23,13 +23,34 @@ food = Budget(200, "Food")
 clothing = Budget(150, "Clothing")
 entertainment = Budget(100, "Entertainment")
 
+# Food Budget inputs
 print("Your food budget for this month is £" + str(food.budget))
 
 spentF = int(
-    input("Please enter the amount you have spent on food this week: £"))
+    input("Please enter food expenditure for the week: £"))
 
 food.withdraw(spentF)
 
 print("Your remaining food budget for this month is £" + str(food.budget))
 
+# Clothing budget inputs
 
+print("Your clothing budget for this month is £" + str(clothing.budget))
+
+spentC = int(
+    input("Please enter clothing expenditure for the week: £"))
+
+clothing.withdraw(spentC)
+
+print("Your remaining food budget for this month is £" + str(clothing.budget))
+
+# Entainment budget Inputs 
+
+print("Your entertainment budget for this month is £" + str(entertainment.budget))
+
+spentE = int(
+    input("Please enter entertainment expenditure for the week: £"))
+
+entertainment.withdraw(spentE)
+
+print("Your remaining entertainment budget for this month is £" + str(entertainment.budget))
