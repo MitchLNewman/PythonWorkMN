96eb332fb34dc0f22e4a6323bb8baaf0d7bf840d
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 <mitchellnewman3@gmail.com>
2023-07-24 12:23:39 +0100

    Added commnets

--- DocumentExamp.py
+++ DocumentExamp.py
@@ -7,7 +7,7 @@ def factorial(x: int) -> int:
         result *= i
     return result
 
-# if __name__ == "__main__" stops print statements from automatically being executed by interpreter
+# if __name__ == "__main__" stops print statements from automatically being executed when this file is imported
 
 if __name__ == "__main__":
     print(factorial(3))
