25669005ab99dd33c9825f466a257888764622a8
Mitch L Newman <mitchellnewman3@gmail.com>
2023-07-25 10:33:15 +0100

    W3 Basics exercises 1-7

---
+++ W3Exercises.py
@@ -0,0 +1,39 @@
+# Pyhton Basics Exercise 1 
+# \n brings the code onto a new line and \t indents the string 
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+# Python Basics Exercise 2 
+# Prints the python version and info with some print statements to make the info more readable
+import sys 
+print("Python Version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
+# Python Basics Exercise 3 
+import datetime 
+now = datetime.datetime.now()
+print("The current date and time is: ")
+print( now.strftime("%Y-%M-%D %H:%M:%S"))
+
+# ython basics Exercise 4
+from math import pi 
+radius =  float(input("Please enter the radius of the circle: "))
+print("The area of a circle with the radius " + str(radius) + " is: " +str(pi * radius**2))
+
+# Python Basics Exercise 5
+firstn = str(input("Please input your first name: "))
+lastn = str(input("Please input your last name: "))
+print( "Hello " + lastn + " " + firstn)
+
+# Python Basics Exercise 6
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
+
+# Python Basics Exercise 7 
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
